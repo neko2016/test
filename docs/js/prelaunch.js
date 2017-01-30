7b787702bc6107217b6e17973565f7cbf1af0589
neko2016 <honglok16@hotmail.com>
2017-01-30 19:24:41 +0000

    fixed the path reference problem

---
+++ docs/js/prelaunch.js
@@ -0,0 +1,8 @@
+var c=document.getElementById("canvas");
+var ctx=c.getContext('2d');
+var my_gradient=ctx.createLinearGradient(0,0,0,140);
+my_gradient.addColorStop(0,"#850405");
+my_gradient.addColorStop(1,"#59532D");
+ctx.fillStyle=my_gradient;
+ctx.fillRect(0,0,2560,1200);
+
